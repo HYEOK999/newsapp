fd9c4febe88ee4398795373e27a8f18069041c6e
KimJunHyeok <iasg2004>
2019-07-02 01:45:30 +0900

    first commit

---
+++ newsapp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsapp
+//
+//  Created by HYEOKBOOK on 02/07/2019.
+//  Copyright © 2019 HYEOKBOOK. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
